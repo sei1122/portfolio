1e5840a7d7305f0f4285e6a2fc701ed1f1f574b3
seiko igi <seikoigi@seikos-MacBook-Pro.local>
2018-11-06 22:41:33 -0800

    this is my portfolio inital

---
+++ js/filter.js
@@ -0,0 +1,27 @@
+$(function () {
+		
+		var filterList = {
+		
+			init: function () {
+			
+				// MixItUp plugin
+				// http://mixitup.io
+				$('#portfoliolist').mixItUp({
+  				selectors: {
+    			  target: '.portfolio',
+    			  filter: '.filter'	
+    		  },
+    		  // load: {
+      		//   filter: '.app'  
+      		// }     
+				});								
+			
+			}
+
+		};
+		
+		// Run the show!
+		filterList.init();
+		
+		
+	});	
