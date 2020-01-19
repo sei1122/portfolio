1afa0dddd7046a77395f0b8cbeeaf28ae7971c66
seiko igi <seikoigi@seikos-MBP.attlocal.net>
2020-01-19 14:06:33 -0800

    fix e-mail

--- submit.php
+++ submit.php
@@ -2,7 +2,8 @@
 
 	$to = "seikoigi@gmail.com"; #set addres to send for to 
 	$subject = "Message from Portfolio"; #set the subject line 
-	$headers = "From: FormMailer@seikoigi.com"; #set the from address 
+	$headers = "From: FormMailer@seikoigi.com". "\r\n" .			
+				"Reply-To: FormMailer@seikoigi.com" . "\r\n"; #set the from address 
 	$forward = 1; # redirect? 1 : yes || 0 : no 
 	$location = "thanks.html"; #set page to redirect to, if 1 is above 
 
@@ -15,10 +16,9 @@
 	foreach ($_POST as $key => $value) { 
 	    $msg .= ucfirst ($key) ." : ". $value . "\n"; 
 	} 
-
-	mail($to, $subject, $msg, $headers); 
+	mail($to, $subject, $msg, $headers, " -fFormMailer@seikoigi.com"); 
 	if ($forward == 1) { 
-	    echo ('<script type="text/javascript">location.href = ' . $location . ';</script>');  
+	    echo ('<script type="text/javascript">location.href = "' . $location . '"</script>');  
 	} 
 	else { 
 	    echo ("Thank you for submitting our form. I will get back to you as soon as possible."); 
