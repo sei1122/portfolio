cc1e2973fb27ffbde4d13fb91a203ef4fa106d1d
seiko igi <seikoigi@gmail.com>
2021-11-12 10:22:46 -0800

    added loading

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 // Hamburger menu start 
 document.getElementById('hamburgler').addEventListener('click', checkNav);
-window.addEventListener("keyup", function(e) {
+window.addEventListener("keyup", function (e) {
   if (e.keyCode == 27) closeNav();
 }, false);
 
@@ -25,85 +25,94 @@ function openNav() {
 
 
 
- // show the sub menu after 800 px from top
- $( document ).ready(function() {
+// show the sub menu after 800 px from top
+$(document).ready(function () {
   $('.subnav').hide();
   $(document).scroll(function () {
-  var y = $(this).scrollTop();
-  if (y > 800) {
+    var y = $(this).scrollTop();
+    if (y > 800) {
       $('.subnav').fadeIn();
-  } else {
+    } else {
       $('.subnav').fadeOut();
-  }
+    }
   });
 });
 
 //sub navigations
 var gPrevToc = null;
 function changeMenu(found) {
-  if(found.length == 0) {
-      return;
+  if (found.length == 0) {
+    return;
   }
   //remove highlight when switch the submenu
   $("ul.submenu li a.active").removeClass("active");
 
-  if(found.hasClass("toc")) {
-      var toc = found;
-      // only update if PrevToc is different than new toc
-      if(gPrevToc == null || toc.text() != gPrevToc.text()) {
-          $("a.toc.active").removeClass("active");
-          var submenu = toc.next();
-          if(gPrevToc != null) {
-              gPrevToc.next().hide();
-              gPrevToc = toc;
-          }
-          else {
-              gPrevToc = toc;
-          }
-          toc.addClass("active");
-          submenu.fadeIn(500);
-          //console.log("f = "+found.text() + " = " + found[0].tagName);
-          //console.log("toc = "+toc.text() + " = " + toc[0].tagName);
-          //console.log("gPrevToc = "+gPrevToc.text() + " = " + gPrevToc[0].tagName);
-          //console.log("submenu = "+submenu.length);
+  if (found.hasClass("toc")) {
+    var toc = found;
+    // only update if PrevToc is different than new toc
+    if (gPrevToc == null || toc.text() != gPrevToc.text()) {
+      $("a.toc.active").removeClass("active");
+      var submenu = toc.next();
+      if (gPrevToc != null) {
+        gPrevToc.next().hide();
+        gPrevToc = toc;
       }
+      else {
+        gPrevToc = toc;
+      }
+      toc.addClass("active");
+      submenu.fadeIn(500);
+      //console.log("f = "+found.text() + " = " + found[0].tagName);
+      //console.log("toc = "+toc.text() + " = " + toc[0].tagName);
+      //console.log("gPrevToc = "+gPrevToc.text() + " = " + gPrevToc[0].tagName);
+      //console.log("submenu = "+submenu.length);
+    }
   } else {
-      var toc = found.parent().parent().parent().find(".toc");
-      found.addClass("active");
+    var toc = found.parent().parent().parent().find(".toc");
+    found.addClass("active");
 
-      // only update if PrevToc is different than new toc
-      if(gPrevToc == null || toc.text() != gPrevToc.text()) {
-          $("a.toc.active").removeClass("active");
-          if(gPrevToc != null) {
-              gPrevToc.next().hide();
-              gPrevToc = toc;
-          }
-          else {
-              gPrevToc = toc;
-          }
-          var submenu = found.closest(".submenu");
-          toc.addClass("active");
-          submenu.fadeIn(500);
-          //console.log("f = "+found.text() + " = " + found[0].tagName);
-          //console.log("toc = "+toc.text() + " = " + toc[0].tagName);
-          //console.log("gPrevToc = "+gPrevToc.text() + " = " + gPrevToc[0].tagName);
-          //console.log("submenu = "+submenu.length);
+    // only update if PrevToc is different than new toc
+    if (gPrevToc == null || toc.text() != gPrevToc.text()) {
+      $("a.toc.active").removeClass("active");
+      if (gPrevToc != null) {
+        gPrevToc.next().hide();
+        gPrevToc = toc;
       }
+      else {
+        gPrevToc = toc;
+      }
+      var submenu = found.closest(".submenu");
+      toc.addClass("active");
+      submenu.fadeIn(500);
+      //console.log("f = "+found.text() + " = " + found[0].tagName);
+      //console.log("toc = "+toc.text() + " = " + toc[0].tagName);
+      //console.log("gPrevToc = "+gPrevToc.text() + " = " + gPrevToc[0].tagName);
+      //console.log("submenu = "+submenu.length);
+    }
   }
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
   //mounse enter the project area, highlight the menu
-  $("section").mouseenter(function(){
-      var id = $(this).attr('id');
-      var found = $("[href=#"+id+"]");
-      changeMenu(found);
-      });
- 
-  //when click the toc, hightlight the menu
-  $(".toc,.submenu a").click(function(){         
-      var found = $(this);
-      changeMenu(found);
+  $("section").mouseenter(function () {
+    var id = $(this).attr('id');
+    var found = $("[href=#" + id + "]");
+    changeMenu(found);
   });
+
+  //when click the toc, hightlight the menu
+  $(".toc,.submenu a").click(function () {
+    var found = $(this);
+    changeMenu(found);
+  });
+});
+
+
+// this is for loading
+//paste this code under the head tag or in a separate js file.
+// Wait for window load
+$(window).load(function () {
+  // Animate loader off screen
+  $(".se-pre-con").fadeOut("slow");;
 });
 
