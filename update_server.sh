115f1d7cbe54d167cf122074dc4e5c2870c54927
Seiko <seikoigi@gmail.com>
2022-04-17 19:24:42 -0700

    Force the config when running udpate script

--- update_server.sh
+++ update_server.sh
@@ -1,3 +1,4 @@
+gcloud config configurations activate seikoigi
 gcloud compute scp cloudscript-kill-server.sh seikoigi_gmail_com@instance-1:./
 echo running cloudscript-kill-server.sh on remote computer
 gcloud compute ssh seikoigi_gmail_com@instance-1 --command="source ~/cloudscript-kill-server.sh 2>&1 < /dev/null | tee ~/log/kill-server.txt"
